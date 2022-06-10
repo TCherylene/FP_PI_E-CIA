9f7faf2feac03b6060915d1dd0489607c423580f
Chery <cherylene40@gmail.com>
2022-06-10 23:17:34 +0700

    top up berhasil

--- topup.js
+++ topup.js
@@ -1,36 +1,34 @@
-// function parseJwt(token) {
-//     var base64Payload = token.split('.')[1];
-//     var payload = from(base64Payload, 'base64');
-//     return JSON.parse(payload.toString()).rows[0];
-// }
+function parseJwt (token) {
+  var base64 = token.split('.')[1];
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
 
-// const parseJwt = require("./parsejwt");
+  return JSON.stringify(jsonPayload);
+};
 
-// const jwt_decode = require('jwt-decode')
-require([
-  "./parsejwt"
-])
 const ecia = JSON.stringify(localStorage.getItem('ecia'));
-console.log(ecia)
-// window.localStorage.setItem('ecia', ecia.token);
-var dataToken = parsejwt(ecia)
-console.log(dataToken)
-var url = "http://localhost:8000/api/profile/";
 
-var myHeaders = new Headers();
-myHeaders.append("Authorization", `Bearer ${ecia}`);
+// Ambil Data Token
+var dataToken = JSON.parse(JSON.parse(parseJwt(ecia))).rows[0]
 
+// Untuk Fetch
 var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer " + ecia);
+
+var token = ("Bearer " + ecia).replace(/\"/g, "");
+
+myHeaders.append("Authorization", token);
 myHeaders.append("Content-Type", "application/json");
 
-const id_user = document.querySelector("#id_user");
+const id_user = document.querySelector("#id_user")
 const nominal = document.querySelector("#nominal");
 const buttonSubmit = document.querySelector("#submit");
 
-
 buttonSubmit.addEventListener("click", (e) => {
     e.preventDefault(); // mencegah refresh
+
+    var url = "http://localhost:8000/api/profile/" + id_user.value
+    console.log(url)
 
     var raw = JSON.stringify({
       jumlah: nominal.value
@@ -42,24 +40,29 @@ buttonSubmit.addEventListener("click", (e) => {
       body: raw,
       redirect: 'follow'
     };
-    
-    fetch(url, requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+
+    async function getResponse(){
+      try {
+          let res = await fetch(url, requestOptions)
+          console.log("Fetch berhasil");
+          return await (res.text());
+      } catch(error) {
+          console.log('error', error)
+      };
+    }
+
     async function getData(){
-        let data = await getResponse();
+      let data = await getResponse();
 
-        var dataJSON = JSON.parse(data);
-        
-        if(dataJSON.status == 200){
-            window.localStorage.setItem('ecia', dataJSON.token);
-            window.location.href = "home.html";
-        }
+      var dataJSON = JSON.parse(data);
 
-        if(dataJSON.status == 400){
-            alert("Terdapat kesalahan silahkan coba lagi");
-        }
+      if(dataJSON.status == 200){
+          alert("Transfer berhasil")
+      }
+
+      if(dataJSON.status == 400){
+          alert(dataJSON.message);
+      }
     };
 
     getData();
