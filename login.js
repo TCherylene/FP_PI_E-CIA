8d0642137086b48d90c1f27f44092d01c72e91a2
Chery <cherylene40@gmail.com>
2022-06-12 15:37:18 +0700

    nambah url sarah dan abad

--- login.js
+++ login.js
@@ -1,5 +1,7 @@
-var url = "https://api-ecia.herokuapp.com/api/login";
+var urlEcia = "https://api-ecia.herokuapp.com/api/login";
 // var url = "http://localhost:8000/api/login";
+var urlSarah = "https://https://moneygo-api.herokuapp.com/api/login"
+var urlAbad = "https://met4kantin.herokuapp.com/api/login"
 
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
@@ -7,7 +9,6 @@ myHeaders.append("Content-Type", "application/json");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const buttonSubmit = document.querySelector("#submit");
-
 
 // email.innerText = (nilai yang mau ditampilkan disini) harus dideklrasi terlebih dahulu 
 
@@ -26,7 +27,7 @@ buttonSubmit.addEventListener("click", (e) => {
         redirect: 'follow'
     };
 
-    async function getResponse(){
+    async function getResponse(url){
         try {
             let res = await fetch(url, requestOptions)
             console.log("Fetch berhasil");
@@ -37,16 +38,22 @@ buttonSubmit.addEventListener("click", (e) => {
     }
 
     async function getData(){
-        let data = await getResponse();
+        let dataEcia = await getResponse(urlEcia);
+        let dataSarah = await getResponse(urlSarah);
+        let dataAbad = await getResponse(urlAbad);
 
-        var dataJSON = JSON.parse(data);
+        var dataJSONEcia = JSON.parse(dataEcia);
+        var dataJSONSarah = JSON.parse(dataSarah);
+        var dataJSONAbad = JSON.parse(dataAbad);
         
-        if(dataJSON.status == 200){
-            window.localStorage.setItem('ecia', dataJSON.token);
+        if(dataJSONEcia.status == 200 && dataJSONSarah.status == 200 && dataAbad){
+            window.localStorage.setItem('ecia', dataJSONEcia.token);
+            window.localStorage.setItem('sarah', dataJSONSarah.token);
+            window.localStorage.setItem('abad', dataJSONAbad.token);
             window.location.href = "home.html";
         }
 
-        if(dataJSON.status == 400){
+        if(dataJSONEcia.status == 400 || dataJSONSarah.status == 400 || dataJSONAbad.status == 400){
             alert("Terdapat kesalahan silahkan coba lagi");
         }
     };
--- register.js
+++ register.js
@@ -1,6 +1,7 @@
-var url = "https://api-ecia.herokuapp.com/api/profile";
-// var url = "http://localhost:8000/api/profile";
-
+var urlEcia = "https://api-ecia.herokuapp.com/api/profile";
+// var url = "http://localhost:8000/api/login";
+var urlSarah = "https://https://moneygo-api.herokuapp.com/api/register"
+var urlAbad = "https://met4kantin.herokuapp.com/api/profile"
 
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
@@ -15,10 +16,9 @@ buttonSubmit.addEventListener("click", (e) => {
     const pass = document.querySelector("#pass");
 
     var raw = JSON.stringify({
-
-    name: nama.value,
-    email: email.value,
-    pass: pass.value
+        name: nama.value,
+        email: email.value,
+        pass: pass.value
     });
 
     var requestOptions = {
@@ -28,7 +28,21 @@ buttonSubmit.addEventListener("click", (e) => {
         redirect: 'follow'
     };
 
-    async function getResponse(){
+    var raw2 = JSON.stringify({
+        name: nama.value,
+        email: email.value,
+        password: pass.value,
+        balance: 0
+    });
+
+    var requestOptions2 = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw2,
+        redirect: 'follow'
+    };
+
+    async function getResponse(url){
         try {
             let res = await fetch(url, requestOptions)
             console.log("Fetch berhasil");
@@ -38,16 +52,30 @@ buttonSubmit.addEventListener("click", (e) => {
         };
     }
 
-    async function getData(){
-        let data = await getResponse();
+    async function getResponse2(url){
+        try {
+            let res = await fetch(url, requestOptions2)
+            console.log("Fetch berhasil");
+            return await (res.text());
+        } catch(error) {
+            console.log('error', error)
+        };
+    }
 
-        var dataJSON = JSON.parse(data);
+    async function getData(){
+        let dataEcia = await getResponse(urlEcia);
+        let dataSarah = await getResponse2(urlSarah);
+        let dataAbad = await getResponse(urlAbad);
+
+        var dataJSONEcia = JSON.parse(dataEcia);
+        var dataJSONSarah = JSON.parse(dataSarah);
+        var dataJSONAbad = JSON.parse(dataAbad);
         
-        if(dataJSON.status == 200){
+        if(dataJSONEcia.status == 200 && dataJSONAbad.status == 200 && dataJSONSarah.status == 200){
             window.location.href = "login.html";
         }
 
-        if(dataJSON.status == 400){
+        if(dataJSON.status == 400 && dataJSONAbad.status == 400 && dataJSONSarah.status == 400){
             alert(dataJSON.message);
         }
     };
